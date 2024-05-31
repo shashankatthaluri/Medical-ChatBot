6aab2a73954816ce384decab49d6fb0619ad0454
shashankatthaluri <shashankatthaluri.com>
2024-05-31 14:11:17 +0530

    version 0.0.1 update

--- setup.py
+++ setup.py
@@ -5,6 +5,13 @@ setup(
     version="0.0.1",
     author="Shashank atthaluri",
     author_email="shashankatthaluri@gmail.com",
-    install_requires=["ctransformers==0.2.5","sentence-transformers==2.2.2", "pinecone-client", "langchain", "flask"],
+    install_requires=["ctransformers==0.2.5",
+                      "sentence-transformers==2.2.2", 
+                      "pinecone-client", "langchain", 
+                      "flask", 
+                      "python-dotenv", 
+                      "langchain-community", 
+                      "pypdf"
+                      ],
     packages=find_packages()         
 )
