1a35cfe19c2cef9f88472ee64e5b1a5a7cf66910
shashankatthaluri <shashankatthaluri.com>
2024-05-31 09:47:02 +0530

    few changes made to requirements

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version="0.0.1",
     author="Shashank atthaluri",
     author_email="shashankatthaluri@gmail.com",
-    packages=find_packages(),
-    install_requires=["ctransformers==0.2.5","sentence-transformers==2.2.2", "pinecone-client", "langchain==0.0.255", "flask"]         
+    install_requires=["ctransformers==0.2.5","sentence-transformers==2.2.2", "pinecone-client", "langchain", "flask"],
+    packages=find_packages()         
 )
